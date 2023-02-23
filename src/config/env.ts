f9df63edab644edca41d89ea9baba537e1b2529e
Jonathan Neidel <jneidel@jneidel.com>
2023-02-23 14:34:23 +0100

    Add local development database

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,11 +1,20 @@
 import { join } from "path";
 
+export let NODE_ENV: "dev"|"prod" = "dev";
+if ( !process.argv[1].match( /\/bin\/run/ ) )
+  NODE_ENV = "prod";
+if ( process.env.NODE_ENV === "dev" )
+  NODE_ENV = "dev";
+else if ( process.env.NODE_ENV === "prod" )
+  NODE_ENV = "prod";
+
 const { HOME } = process.env;
 if ( !HOME ) {
   console.error( "Environmental variable $HOME is not set" );
   process.exit( 1 );
 }
 
-export const DATA_DIR = join( HOME, ".local/share/pots" );
+export const DATA_DIR = NODE_ENV === "dev" ?
+  join( __dirname, "..", ".." ) :
+  join( HOME, ".local/share/pots" );
 export const SQLITE_DB = join( DATA_DIR, "db.sqlite" );
-export const TRANSACTIONS_FILE = ( year: string|number ) => join( DATA_DIR, `transactions-${year}.json` );
